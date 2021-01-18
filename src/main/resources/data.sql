15e6d43331533fd91cbbd8d3d7e460460e731036
Yuta <y.sugiyama54@gmail.com>
2021-01-18 23:45:07 +0900

    DDLとDML

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+/∗ 従業員テーブルのデータ(第3章で作成) ∗/
+INSERT INTO employee (employee_id, employee_name, age) VALUES(1, '山田太郎', 30);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+/∗ 従業員テーブル ∗/
+CREATE TABLE IF NOT EXISTS employee (
+employee_id INT PRIMARY KEY, employee_name VARCHAR(50), age INT
+);
